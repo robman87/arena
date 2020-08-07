9d71bdb67ac3725ce685c02fb41a57fe502293ab
Robert Michalski <robert@surveylegend.com>
2020-08-07 12:10:00 +0200

    Run both one instance for bull and another for bee queue.

--- server.example.js
+++ server.example.js
@@ -1,30 +1,43 @@
-// Use to run bull-arena while developing bull-arena
+// Use to run bull-arena while developing this package
 
 //const Arena = require('bull-arena'); // Use in production environment
-const Arena = require('./index'); //
+const Arena = require('./index'); // Use when developing this package
 
 // Mandatory import of queue library.
 const Bull = require('bull');
+const Bee = require('bee-queue');
 
-Arena({
-    // All queue libraries used must be explicitly imported and included.
-    Bull,
+const bullApp = Arena(
+    {
+        Bull,
+        queues: [
+            {
+                // Required for each queue definition.
+                name: 'name_of_my_bull_queue',
+                // User-readable display name for the host. Required.
+                hostId: 'Queue Server 1',
+                // Queue type (Bull or Bee - default Bull).
+                //type: 'bull',
+                // Queue key prefix. Defaults to "bq" for Bee and "bull" for Bull.
+                //prefix: 'bull',
+            },
+        ],
+    }
+);
 
-    // Set `Bee` when using bee.
-
-    queues: [
-        {
-            // Required for each queue definition.
-            name: 'name_of_my_queue',
-
-            // User-readable display name for the host. Required.
-            hostId: 'Queue Server 1',
-
-            // Queue type (Bull or Bee - default Bull).
-            type: 'bull',
-
-            // Queue key prefix. Defaults to "bq" for Bee and "bull" for Bull.
-            prefix: 'foo',
-        },
-    ],
-});
+const beeApp = Arena(
+    {
+        Bee,
+        queues: [
+            {
+                // Required for each queue definition.
+                name: 'name_of_my_bee_queue',
+                // User-readable display name for the host. Required.
+                hostId: 'Queue Server 1',
+                type: 'bee',
+                //prefix: 'bq',
+            },
+        ],
+    },
+    { port: 4568 }
+);
