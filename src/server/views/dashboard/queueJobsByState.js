320026857bd3331c2eb5e1bcc88c0a79156b0756
Robert Michalski <robert@surveylegend.com>
2020-08-07 13:02:37 +0200

    Filter invalid bull jobs before rendering.

--- src/server/views/dashboard/queueJobsByState.js
+++ src/server/views/dashboard/queueJobsByState.js
@@ -101,6 +101,7 @@ async function _html(req, res) {
     jobs = jobs.filter((job) => job);
   } else {
     jobs = await queue[`get${_.capitalize(state)}`](startId, endId);
+    jobs = jobs.filter((job) => job && typeof job === 'object') // Filter out Bull jobs that have become null by the time the promise resolves
     await jobs.map(async (job) => {
       let logs = await queue.getJobLogs(job.id);
       job.logs = logs.logs || 'No Logs';
